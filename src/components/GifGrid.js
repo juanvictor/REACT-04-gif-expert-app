583322d38c7b935eac38ead945cb7dbfce403a18
JUAN VICTOR SERRUDO CHAVEZ <juan.vsc@gmail.com>
2020-10-05 22:34:00 -0400

    20201005 Avances del dia

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export const GifGrid = ({ category }) => {
+
+    const [ count, setCount ] = useState( 0 );
+
+    useEffect( () => {
+        getGifs();
+    }, []);
+
+    const getGifs = async () => {
+
+        const url = 'https://api.giphy.com/v1/gifs/trending?q=Rick+and+Morty&limit=10&api_key=1bU0m0vKaBd8fvebUEC3UhZoZ2s5hE6G';
+        const resp = await fetch( url );
+        const { data } = await resp.json();
+
+        const gifs = data.map( img => {
+            return {
+                id: img.id,
+                title: img.title,
+                url: img.images?.downsized_medium.url
+            }
+        });
+
+        console.log(gifs);
+
+    }
+
+
+    // getGifs();
+
+
     return (
         <div>
             <h3> { category } </h3>
+            <h3>{ count}</h3>
+            <button onClick={ () => setCount( count + 1 ) }></button>
         </div>
     )
 }
