291c88db44f8bcc487b3461291b0e992d5a12198
JUAN VICTOR SERRUDO CHAVEZ <juan.vsc@gmail.com>
2020-10-10 22:13:12 -0400

    20201010 Se termino la app

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,40 +1,28 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { useFetchGifs } from '../hooks/useFetchGifs';
+import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
 
-    const [ count, setCount ] = useState( 0 );
-
-    useEffect( () => {
-        getGifs();
-    }, []);
-
-    const getGifs = async () => {
-
-        const url = 'https://api.giphy.com/v1/gifs/trending?q=Rick+and+Morty&limit=10&api_key=1bU0m0vKaBd8fvebUEC3UhZoZ2s5hE6G';
-        const resp = await fetch( url );
-        const { data } = await resp.json();
-
-        const gifs = data.map( img => {
-            return {
-                id: img.id,
-                title: img.title,
-                url: img.images?.downsized_medium.url
-            }
-        });
-
-        console.log(gifs);
-
-    }
-
-
-    // getGifs();
-
+    const {data:images, loading} = useFetchGifs( category );    // Para renombrar una variable data:images
 
     return (
-        <div>
-            <h3> { category } </h3>
-            <h3>{ count}</h3>
-            <button onClick={ () => setCount( count + 1 ) }></button>
-        </div>
+        <>
+            <h3 className="animate__animated animate__fadeIn"> { category } </h3>
+
+            { loading && <p className="animate__animated animate__flash">Loading</p> }
+
+            <div className =" card-grid">
+                {
+                    images.map( img => (
+                        <GifGridItem
+                            key={ img.id }
+                            { ...img }
+                        />
+                    ))
+                }
+
+            </div>
+        </>
     )
 }
